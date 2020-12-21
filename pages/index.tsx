fbb561c2975915842bd9fd902f76e7391dd8a20b
Kseniia Stativka <kseniia.stativka@gmail.com>
2020-12-21 11:21:58 +0100

    chore: mock home page

---
+++ pages/index.tsx
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+
+const mockData = ['City1', 'City2', 'City3']
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>top-tops</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Ratings!
+        </h1>
+
+          <article>
+            <header>Top German cities</header>
+
+            <ul>
+              {mockData.map(item=><li>{item}</li>)}
+            </ul>
+          </article>
+
+      </main>
+
+
+    </div>
+  )
+}
