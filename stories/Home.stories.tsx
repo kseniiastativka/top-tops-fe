1f30ada242be9f060a0ba196585a31d437aeabf8
Eugene Stativka <stativkaea@gmail.com>
2020-12-25 19:32:44 +0100

    chore: fix Storybook

--- stories/Home.stories.tsx
+++ stories/Home.stories.tsx
@@ -2,6 +2,7 @@ import { Story, Meta } from "@storybook/react/types-6-0"
 import type { User } from "../components/UserProvider"
 import Home from "../pages"
 import { createRootComponent } from "./createRootComponent"
+import { PUBLIC_RATINGS_MOCK } from "../mocks/public-ratings.mock"
 
 const HomePage = createRootComponent(Home)
 
@@ -17,6 +18,10 @@ const Template: Story<{ user: User }> = (args) => <HomePage {...args} />
 
 export const LoggedOut = Template.bind({})
 
+LoggedOut.args = {
+  pageProps: { publicRatings: PUBLIC_RATINGS_MOCK },
+}
+
 LoggedOut.parameters = {
   percy: { skip: false },
 }
@@ -25,6 +30,7 @@ export const LoggedIn = Template.bind({})
 
 LoggedIn.args = {
   user: { name: "StorybookUser", id: "1", token: "token" },
+  pageProps: { publicRatings: PUBLIC_RATINGS_MOCK },
 }
 
 LoggedIn.parameters = {
