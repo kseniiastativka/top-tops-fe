cfd84447c47666a5d0333657c575e4e5209b7f1b
Kseniia Stativka <kseniia.stativka@gmail.com>
2020-12-25 14:07:33 +0100

    chore: address PR comments

--- cypress/integration/login.spec.ts
+++ cypress/integration/login.spec.ts
@@ -1,16 +1,16 @@
-import { Rick, UserWithInvalidPassword } from "../constants/users"
+import { RICK, USER_WITH_INVALID_PASSWORD } from "../constants/users"
 import { login } from "../page-objects/login.page"
 
 describe("Login", () => {
   it("is successful with valid credentials ", () => {
-    login.logIn(Rick)
+    login.logIn(RICK)
 
     cy.contains("Profile")
     cy.url().should("not.include", "/login")
   })
 
   it("fails with invalid credentials", function () {
-    login.logIn(UserWithInvalidPassword)
+    login.logIn(USER_WITH_INVALID_PASSWORD)
 
     cy.contains("Login failed")
     cy.url().should("include", "/login")
