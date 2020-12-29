db9fd446d00244baefa1471d619db4973c71a104
Eugene Stativka <stativkaea@gmail.com>
2020-12-29 11:31:14 +0100

    chore: fix mock

--- mocks/public-ratings.mock.ts
+++ mocks/public-ratings.mock.ts
@@ -3,14 +3,20 @@ import type { RatingModel } from "../models/ratingModel"
 export const PUBLIC_RATINGS_MOCK: Array<RatingModel> = [
   {
     id: "1",
-    items: [{ name: "First item" }, { name: "Second item" }],
+    items: [
+      { name: "First item", id: "1" },
+      { name: "Second item", id: "2" },
+    ],
     name: "First rating",
     author: { id: "1", name: "First author" },
     visibility: "public",
   },
   {
     id: "2",
-    items: [{ name: "First item" }, { name: "Second item" }],
+    items: [
+      { name: "First item", id: "1" },
+      { name: "Second item", id: "1" },
+    ],
     name: "Second rating",
     author: { id: "2", name: "Second author" },
     visibility: "public",
